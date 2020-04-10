161f4a4a907f7fce34ae9996f40c7e9b55e73f4d
Ali Rıza KESKİN <parduscix@yandex.ru>
2020-04-10 23:08:06 +0000

    isim değişikliği

--- tekel.sh
+++ tekel.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 [ $UID -eq 0 ] && echo "root not allowed" && exit 1
-PATH=$PATH:/usr/libexec/bakkal/cli
-mkdir -p $HOME/.bakkal/{tmp,packages}
+PATH=$PATH:/usr/libexec/tekel/cli
+mkdir -p $HOME/.tekel/{tmp,packages}
 [ "$1" == "install" ] && b_it $*
 [ "$1" == "remove" ] && b_rm $*
 [ "$1" == "info" ] && b_inf $*
